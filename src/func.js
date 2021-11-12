3746c669a7f55d08e154ac703ed08a3a3a98a864
Aman Singh <besoeasy@users.noreply.github.com>
2021-11-12 11:36:54 +0400

    commit 1

---
+++ src/func.js
@@ -0,0 +1,50 @@
+const node = 'https://vault.nanocrawler.cc/api/node-api';
+const gameaddress = 'nano_3zrie5gc4j8r536d3hj3pa48aejkskkgrec8f961xc78cmsw6uamx5z3q5xq';
+
+import axios from 'axios';
+import sha256 from 'js-sha256';
+
+function sha256tofloat(hash) {
+	hash = sha256(hash);
+	hash = hash.substring(0, 8);
+	hash = parseInt(hash, 16);
+	hash = hash / 0xffffffff;
+	return hash * 100;
+}
+
+async function blockInfo(block) {
+	const response = await axios.post(node, { json_block: 'true', action: 'blocks_info', hashes: [block] }).then(function(response) {
+		return response.data.blocks[block];
+	});
+
+	return response;
+}
+
+async function fetchData() {
+	const response = await axios
+		.post(node, {
+			action: 'account_history',
+			account: gameaddress,
+			count: 100,
+		})
+		.then(function(response) {
+			return response.data;
+		});
+
+	return response;
+}
+
+async function accountfetchData() {
+	const response = await axios
+		.post(node, {
+			action: 'account_info',
+			account: gameaddress,
+		})
+		.then(function(response) {
+			return response.data;
+		});
+
+	return response;
+}
+
+export { sha256tofloat, blockInfo, fetchData, accountfetchData, gameaddress };
---
+++ worker.js
@@ -0,0 +1,65 @@
+const nano = require('nanopay');
+const sha256 = require('js-sha256');
+
+// init nano.init( nanonode, worknode data)
+nano.init('https://vault.nanocrawler.cc/api/node-api', { node: 'https://bpow.banano.cc/service/', user: process.env.USER, api_key: process.env.API_KEY });
+
+const secrateKey = process.env.SECRATE_KEY;
+
+function sha256tofloat(hash) {
+	var hash = sha256(hash);
+	var hash = hash.substring(0, 8);
+	var hash = parseInt(hash, 16);
+	var hash = hash / 0xffffffff;
+	return hash * 100;
+}
+
+async function game() {
+	// generate address
+	var genaddress = await nano.secretKeytoAddress(secrateKey);
+
+	// recieve pending transactions
+	await nano.fetchPending(secrateKey);
+
+	// fetch Data
+	var accountData = await nano.addressInfo(genaddress);
+
+	var lastbet = accountData.history[0] || null;
+
+	var blockinfo = await nano.blockInfo(lastbet.hash);
+
+	console.log(blockinfo);
+
+	if (lastbet.type == 'receive') {
+		var float = sha256tofloat(lastbet.hash + blockinfo.contents.signature + blockinfo.contents.work);
+
+		console.log('Float : ' + float);
+                
+                var amount = Math.min(lastbet.amount * 2, accountData.info.balance / 100) / 1000000000000000000000000000000;
+
+		if (float < 49 && amount > 0.00000001) {
+                var done2 = await nano.send(secrateKey, lastbet.account, amount);
+                console.log(done2);
+		}
+
+		if (float > 90) {
+                var done3 = await nano.send(secrateKey, 'nano_3sumsrd8ckhokkb7379p49c1ndzpi7f94bzkxqozrfs8fk38qc1awn9k1kyb', amount);
+                console.log(done3);
+		}
+
+	} else {
+		console.log('fail safe');
+	}
+}
+
+async function main() {
+	while (true) {
+		try {
+			await game();
+		} catch (e) {
+			console.log(e);
+		}
+	}
+}
+
+main();
