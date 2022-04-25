f93b75294197109b464a3c5ce6d5315ab9c1e298
Aman Singh Katal <besoeasy@users.noreply.github.com>
2022-04-25 12:31:02 +0530

    --

--- src/App.vue
+++ src/App.vue
@@ -19,13 +19,13 @@
 
 		<section class="text-gray-600 body-font">
 			<div class="container mx-auto flex px-5 py-10 items-center justify-center flex-col">
-				<img class="mb-10 object-cover object-center rounded" alt="hero" src="deposit.png" />
+				<img class="mb-10 object-cover object-center rounded" src="deposit.png" />
 				<div class="text-center lg:w-2/3 w-full">
 					<h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">DEPOSIT NANO TO PLAY !</h1>
 					<p class="mb-8 leading-relaxed">
 						{{ gameaddress }}
 					</p>
-					<div class="flex justify-center">
+					<div class="flex justify-center sm:block md:hidden">
 						<a :href="'nano:' + gameaddress" class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Open Natrium</a>
 					</div>
 				</div>
