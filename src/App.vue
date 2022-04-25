67b0b5764ebd59fedd66cb762a1b6698fe4c0271
Aman Singh Katal <besoeasy@users.noreply.github.com>
2022-04-25 12:28:04 +0530

    deposit

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
 
 		<section class="text-gray-600 body-font">
 			<div class="container mx-auto flex px-5 py-10 items-center justify-center flex-col">
-				<img class="mb-10 object-cover object-center rounded" alt="hero" src="./deposit.png" />
+				<img class="mb-10 object-cover object-center rounded" alt="hero" src="deposit.png" />
 				<div class="text-center lg:w-2/3 w-full">
 					<h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">DEPOSIT NANO TO PLAY !</h1>
 					<p class="mb-8 leading-relaxed">
