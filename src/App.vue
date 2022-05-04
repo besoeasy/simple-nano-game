a351b282889734cea8aed2890bce48d3ad8dcf48
besoeasy <besoeasy@users.noreply.github.com>
2022-05-04 07:54:43 +0530

    func removed

--- src/App.vue
+++ src/App.vue
@@ -96,13 +96,69 @@
 </template>
 
 <script>
-	import { gameaddress, fetchData, accountfetchData, hashtoroll } from './func';
+	const nodes = ['https://nault.nanos.cc/proxy', 'https://vault.nanocrawler.cc/api/node-api'];
+
+	const gameaddress = 'nano_3zrie5gc4j8r536d3hj3pa48aejkskkgrec8f961xc78cmsw6uamx5z3q5xq';
+
+	const node = nodes[Math.floor(Math.random() * nodes.length)];
+
+	import axios from 'axios';
+	import sha256 from 'js-sha256';
+
+	function sha256tofloat(hash) {
+		hash = sha256(hash);
+		hash = hash.substring(0, 8);
+		hash = parseInt(hash, 16);
+		hash = hash / 0xffffffff;
+		return hash * 100;
+	}
+
+	async function blockInfo(block) {
+		const response = await axios.post(node, { json_block: 'true', action: 'blocks_info', hashes: [block] }).then(function(response) {
+			return response.data.blocks[block];
+		});
+
+		return response;
+	}
+
+	async function fetchData() {
+		const response = await axios
+			.post(node, {
+				action: 'account_history',
+				account: gameaddress,
+				count: 200,
+			})
+			.then(function(response) {
+				return response.data;
+			});
+
+		return response;
+	}
+
+	async function accountfetchData() {
+		const response = await axios
+			.post(node, {
+				action: 'account_info',
+				account: gameaddress,
+			})
+			.then(function(response) {
+				return response.data;
+			});
+
+		return response;
+	}
+
+	async function hashtoroll(hash) {
+		const blockinfo = await blockInfo(hash);
+
+		return sha256tofloat(hash + blockinfo.contents.signature + blockinfo.contents.work).toFixed(2);
+	}
 
 	export default {
 		data() {
 			return {
 				gameaddress: gameaddress,
-				balance: 1000000000000000000000000000000*1353,
+				balance: 1000000000000000000000000000000 * 1353,
 				betsdata: [],
 			};
 		},
@@ -140,7 +196,7 @@
 			},
 		},
 		async mounted() {
-                      this.fetchdata();
+			this.fetchdata();
 		},
 	};
 </script>
